0c1fed52f9ce7f1374f09cf339b855a1c7067dd9
CorneliusDrebbel <darkorion89@gmail.com>
2020-12-22 11:27:13 +0300

    upd

--- devops_14_1/config.tf
+++ devops_14_1/config.tf
@@ -6,59 +6,19 @@ terraform {
   }
 }
 
-# Define access and secret key for aws in variables
-variable "my_access_key" {
-  default = "enter-your-access-key-here!!!!!!!!!!!!"
-} 
-variable "my_secret_key" {
-  default = "enter-your-secret-key-here!!!!!!!!!!!!"
-} 
-
-# Define my bucket name in a variable
-variable "my_bucket" {
-  default = "s3://mybacket1.ufndtwocmeu.ru"
-} 
-
-# Define instance type in a variable
-variable "inst_type" {
-  default = "t2.micro"
-} 
-
-# Define region in a variable
-variable "deploy_region" {
-  default = "us-east-2"
-} 
-
-# Define varibale for EC2 ubuntu18.04 image for us-east-2 aws region
-variable "image_id" {
-  default = "ami-0dd9f0e7df0f0a138"
-}
-
-# Define VPC in a variable
-variable "my_vpc_id" {
-  default = "vpc-8d8f30e6"
-}
-
-
-
-# Configure the AWS Provider
 provider "aws" {
   region = var.deploy_region
   access_key = var.my_access_key
   secret_key = var.my_secret_key
 }
 
-
-
-
-# Create security rules for deploy
-resource "aws_security_group" "allow_tomcat" {
-  name        = "allow_tomcat"
+resource "aws_security_group" "tomcat_ssh" {
+  name        = "allow_tomcat_ssh"
   description = "Allow traffic for Tomcat"
   vpc_id      = var.my_vpc_id
 
   ingress {
-    description = "Incoming tcp at 8080 from everywhere"
+    description = "tcp in 8080"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -66,27 +26,36 @@ resource "aws_security_group" "allow_tomcat" {
   }
 
   egress {
-    description = "Allow all outcoming traffic"
+    description = "all out"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    description = "management"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "allow_tomcat"
+    Name = "tomcat_ssh"
   }
 }
 
 
 
-
-
-# Create buider instance
 resource "aws_instance" "build_instance" {
+  tags = {
+    Name = "build"
+  }
+  key_name = var.my_key_pair
   ami = var.image_id
   instance_type = var.inst_type
-  vpc_security_group_ids = [aws_security_group.allow_tomcat.id]
+  vpc_security_group_ids = [aws_security_group.tomcat_ssh.id]
   user_data = <<EOF
 #!/bin/bash
 sudo apt update && sudo apt install -y openjdk-8-jdk maven git awscli
@@ -97,14 +66,22 @@ export AWS_SECRET_ACCESS_KEY=${var.my_secret_key}
 export AWS_DEFAULT_REGION=${var.deploy_region}
 aws s3 cp /data/boxfuse-sample-java-war-hello/target/hello-1.0.war ${var.my_bucket}
 EOF
-  
 }
 
-# Create production instance
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [aws_instance.build_instance]
+  create_duration = "60s"
+}
+
 resource "aws_instance" "prod_instance" {
+  tags = {
+    Name = "prod"
+  }
+  key_name = var.my_key_pair
   ami = var.image_id
   instance_type = var.inst_type
-  vpc_security_group_ids = [aws_security_group.allow_tomcat.id]
+  vpc_security_group_ids = [aws_security_group.tomcat_ssh.id]
+  depends_on = [time_sleep.wait_60_seconds]
   user_data = <<EOF
 #!/bin/bash
 sudo apt update && sudo apt install -y openjdk-8-jdk tomcat8 awscli
@@ -113,6 +90,5 @@ export AWS_SECRET_ACCESS_KEY=${var.my_secret_key}
 export AWS_DEFAULT_REGION=${var.deploy_region}
 aws s3 cp ${var.my_bucket}/hello-1.0.war /var/lib/tomcat8/webapps/hello-1.0.war
 sudo systemctl restart tomcat8
-EOF
-  
+EOF  
 }
---
+++ devops_14_1/variables.tf
@@ -0,0 +1,30 @@
+variable "my_access_key" {
+  default = "AKIAYRZAMWJSQO5SZKXI"
+} 
+variable "my_secret_key" {
+  default = "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"
+} 
+
+variable "my_key_pair" {
+  default = "devops"
+}
+
+variable "my_bucket" {
+  default = "s3://mybucket11.test.com"
+} 
+
+variable "inst_type" {
+  default = "t2.micro"
+} 
+
+variable "deploy_region" {
+  default = "eu-central-1"
+} 
+
+variable "image_id" {
+  default = "ami-0e1ce3e0deb8896d2"
+}
+
+variable "my_vpc_id" {
+  default = "vpc-b765d3dd"
+}
